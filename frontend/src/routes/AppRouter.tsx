5a62cdeadf8ea0af6ea5b3bf8c878a6f29735b4f
Francisca Figueroa <fran@baiko.cl>
2025-10-16 20:59:19 -0300

    feat: add new route to router

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DetailPage from '../pages/DetailPage/DetailPage';
+import LotesGestionPage from '../pages/LotesGestionPage';
 import LotesPage from '../pages/LotesPage';
 
 export default function AppRouter() {
@@ -9,6 +10,7 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<LotesPage />} />
         <Route path="/:id" element={<DetailPage />} />
+        <Route path="/lotes-gestion" element={<LotesGestionPage />} />
       </Routes>
     </BrowserRouter>
   )
