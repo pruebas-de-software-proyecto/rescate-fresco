70989c22b1f2153ffa51b9dbc50b5753f2c35387
Francisca Figueroa <fran@baiko.cl>
2025-11-08 17:36:44 -0300

    feat: add url api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = 'https://rescate-fresco-addhaeh7cbehd5ad.eastus2-01.azurewebsites.net/api';
 // --------------------
 
 const api = axios.create({
