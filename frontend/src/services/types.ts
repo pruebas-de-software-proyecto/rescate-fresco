6819f6b1eb5fef001c4ad4147df443eac9519d2a
Francisca Figueroa <fran@baiko.cl>
2025-11-09 00:58:08 -0300

    fix: url api

--- frontend/src/services/types.ts
+++ frontend/src/services/types.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.PROD 
+  ? 'https://rescate-fresco-addhaeh7cbehd5ad.eastus2-01.azurewebsites.net/api' 
+  : 'http://localhost:5000/api';
 // --------------------
 
 export interface FullLote {
