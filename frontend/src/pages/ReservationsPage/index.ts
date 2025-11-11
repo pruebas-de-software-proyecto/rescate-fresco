f2850e51da8a3f5b87e81a6f279d15d862bf61f3
Francisca Figueroa <fran@baiko.cl>
2025-11-10 22:52:30 -0300

    feat: se añade pagina de reservación

---
+++ frontend/src/pages/ReservationsPage/index.ts
@@ -0,0 +1,2 @@
+export { default as ReservationsPage } from './ReservationsPage';
+
--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DetailPage from '../pages/DetailPage/DetailPage';
 import LotesGestionPage from '../pages/LotesGestionPage';
 import LotesPage from '../pages/LotesPage';
+import { ReservationsPage } from '../pages/ReservationsPage';
 
 export default function AppRouter() {
   return (
@@ -10,6 +11,7 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<LotesPage />} />
         <Route path="/:id" element={<DetailPage />} />
+        <Route path="/reservation" element={<ReservationsPage />} />
         <Route path="/lotes-gestion" element={<LotesGestionPage />} />
       </Routes>
     </BrowserRouter>
